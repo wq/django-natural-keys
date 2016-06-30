30736b4e7b4c04b80015797afbeea1cc417f5cc4
S. Andrew Sheppard <andrew@wq.io>
2016-06-30 16:32:31 -0500

    update version for dev

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def parse_markdown_readme():
 
 setup(
     name='natural-keys',
-    version='0.1.0',
+    version='0.1.1-dev',
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://github.com/wq/django-natural-keys',
