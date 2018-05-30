e3f3c1ef62f849bd4e3c041f1818ab549a0d1234
Arvind Chembarpu <arvindchembarpu@gmail.com>
2018-05-30 17:50:58 +0200

    Export ModelManager & QuerySet as well

--- natural_keys/__init__.py
+++ natural_keys/__init__.py
@@ -1,7 +1,8 @@
-from .models import NaturalKeyModel
+from .models import NaturalKeyModel, NaturalKeyModelManager, NaturalKeyQuerySet
 from .serializers import NaturalKeySerializer, NaturalKeyModelSerializer
 
 
 __all__ = [
-    'NaturalKeyModel', 'NaturalKeySerializer', 'NaturalKeyModelSerializer',
+    'NaturalKeyModel', 'NaturalKeyModelManager', 'NaturalKeySerializer',
+    'NaturalKeyModelSerializer', 'NaturalKeyQuerySet'
 ]
